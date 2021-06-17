8ce20d895bc290eeb4b6e826e3406d25bfc9cd9a
nmacianx <nicoomacian@gmail.com>
2021-06-17 12:08:20 +0300

    Scraper refractored to function based

--- scraper.py
+++ scraper.py
@@ -7,26 +7,25 @@ import os
 BASE_URL = "https://www.cnet.com/news/"
 DOMAIN_URL = "https://www.cnet.com"
 
-page = requests.get(BASE_URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-scrape_urls = []
-# print(soup.prettify())
 
-top_stories = soup.select('#topStories > div > a')
-scrape_urls += [DOMAIN_URL + a.get('href') for a in top_stories]
+def scrape_main_page():
+    scrape_urls = []
+    page = requests.get(BASE_URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-more_top_stories = soup.find(class_='moreTopStories')
-more_links = more_top_stories.select('.assetBody > a')
-scrape_urls += [DOMAIN_URL + a.get('href') for a in more_links]
-# We have all the top stories URLs
-print('{} stories will be scraped'.format(len(scrape_urls)))
+    top_stories = soup.select('#topStories > div > a')
+    scrape_urls += [DOMAIN_URL + a.get('href') for a in top_stories]
 
-# Now we scrape each story
-results = []
-for ix, news in enumerate(scrape_urls):
-    print('Scraping story no. {}...'.format(ix + 1))
+    more_top_stories = soup.find(class_='moreTopStories')
+    more_links = more_top_stories.select('.assetBody > a')
+    scrape_urls += [DOMAIN_URL + a.get('href') for a in more_links]
+
+    return scrape_urls
+
+
+def scrape_story(url):
     news_content = {}
-    page = requests.get(news)
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     header = soup.select('.content-header .c-head h1.speakableText')
@@ -44,21 +43,46 @@ for ix, news in enumerate(scrape_urls):
     date = soup.select('.content-header .c-assetAuthor_date time')
     if len(date) > 0:
         news_content['date'] = date[0].getText()
-    results.append(news_content)
 
-file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'scraping.txt')
-with open(file_path, 'a') as f:
-    f.write('Scraping session: {}\n'.format(
-        datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")))
-    f.write('====================\n')
-    for ix, news in enumerate(results):
-        f.write('\n\nStory {}:\n'.format(ix + 1))
-        if 'title' in news:
-            f.write('Title: {}\n'.format(news['title']))
-        if 'description' in news:
-            f.write('Description: {}\n'.format(news['description']))
-        if 'authors' in news:
-            authors = ', '.join(news['authors'])
-            f.write('Author/s: {}\n'.format(authors))
-        if 'date' in news:
-            f.write('Published date: {}\n'.format(news['date']))
+    return news_content
+
+
+def scrape_stories(scrape_urls):
+    results = []
+    for ix, news in enumerate(scrape_urls):
+        print('Scraping story no. {}...'.format(ix + 1))
+        content = scrape_story(news)
+        results.append(content)
+    return results
+
+
+def save_results(results):
+    file_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), 'scraping.txt')
+    with open(file_path, 'a') as f:
+        f.write('Scraping session: {}\n'.format(
+            datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")))
+        f.write('====================\n')
+        for ix, news in enumerate(results):
+            f.write('\n\nStory {}:\n'.format(ix + 1))
+            if 'title' in news:
+                f.write('Title: {}\n'.format(news['title']))
+            if 'description' in news:
+                f.write('Description: {}\n'.format(news['description']))
+            if 'authors' in news:
+                authors = ', '.join(news['authors'])
+                f.write('Author/s: {}\n'.format(authors))
+            if 'date' in news:
+                f.write('Published date: {}\n'.format(news['date']))
+        f.write('====================\n\n')
+
+
+def main():
+    urls = scrape_main_page()
+    print('{} stories will be scraped'.format(len(urls)))
+    scrape_results = scrape_stories(urls)
+    save_results(scrape_results)
+
+
+if __name__ == '__main__':
+    main()
