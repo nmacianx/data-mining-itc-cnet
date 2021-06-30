a3ddd39deb9a9a8ccc42d298ff9fd25b3f8a6a90
nmacianx <nicoomacian@gmail.com>
2021-06-30 19:58:29 +0300

    Transformed to OOP implementation

--- settings.py
+++ settings.py
@@ -1,11 +1,16 @@
+# Scraper User Settings
+LOGGING = True
+SHOULD_SAVE = True
+FAIL_SILENTLY = False
+
+# Scraper internal config
 BASE_URL = "https://www.cnet.com/news/"
 DOMAIN_URL = "https://www.cnet.com"
 DESTINATION_FILE_NAME = 'scraping.txt'
 
-NEWS_UNKNOWN_STRUCTURE = "Can't scrape unknown website structure."
 CONSOLE_WELCOME_MESSAGE = 'CNET News Web Scraper initialized'
+ERROR_FILE_PATH = "Error! Path to file_name doesn't exist."
 
-# Scraper config
 CONFIG_PATTERN_COMMON = '#topStories > div > a[href]'
 CONFIG_PATTERN_NUXT_JS = '.moreTopStories .assetBody > a[href]'
 CONFIG_MAIN_PATTERN = [
@@ -32,4 +37,23 @@ CONFIG_TEMPLATE_NUXT_JS = {
 CONFIG_TEMPLATES = [
     CONFIG_TEMPLATE_COMMON,
     CONFIG_TEMPLATE_NUXT_JS
+]
+
+STORY_SCRAPE_FIELDS = [
+    {
+        'field': 'title',
+        'multiple': False,
+    },
+    {
+        'field': 'description',
+        'multiple': False,
+    },
+    {
+        'field': 'authors',
+        'multiple': True,
+    },
+    {
+        'field': 'date',
+        'multiple': False,
+    },
 ]
